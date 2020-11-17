c4e9888d6a6092b2365a8e4e9dff5591a9b1e95e
Godfrey96 <ngwatlegodfrey@gmail.com>
2020-11-17 11:09:58 +0200

    17-11-2020 v1

--- src/app/pages/landing-page/landing-page.page.ts
+++ src/app/pages/landing-page/landing-page.page.ts
@@ -16,8 +16,10 @@ import 'firebase/firestore'
 export class LandingPagePage implements OnInit {
 
   restaurants: Array<any> = [];
+  loadedRestaurants: Array<any> = [];
   // restaurants: any;
   id: any;
+  searchText: string;
 
   slideOpts = {
     initialSlide: 0,
@@ -32,8 +34,33 @@ export class LandingPagePage implements OnInit {
     firebase.firestore().collection('restaurants').onSnapshot(res => {
       res.forEach(element => {
         this.restaurants.push(element.data());
+        this.loadedRestaurants = this.restaurants
+        console.log('List of resta: ', this.loadedRestaurants)
       });
     });
+  }
+
+  initializeItems(): void{
+    this.restaurants = this.loadedRestaurants;
+  }
+
+  filterList(evt){
+    this.initializeItems();
+
+    const searchTerm = evt.srcElement.value;
+
+    if(!searchTerm){
+      return;
+    }
+
+    this.restaurants = this.restaurants.filter(currentRest => {
+      if(currentRest.resName && searchTerm){
+        if(currentRest.resName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1){
+          return true;
+        }
+        return false;
+      }
+    })
   }
 
 }
